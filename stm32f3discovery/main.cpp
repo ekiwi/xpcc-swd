86072473b15ee407c93a95eda38be5f74459c260
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2016-09-30 18:24:18 -0700

    [test] read out stm32f4 unique id and flash size

--- stm32f3discovery/main.cpp
+++ stm32f3discovery/main.cpp
@@ -28,7 +28,7 @@ main()
 	GpioInputA3::connect(Usart2::Rx, Gpio::InputType::PullUp);
 	Usart2::initialize<Board::systemClock, 115200>(12);
 
-	ARMDebug target;
+	ARMDebug target(ARMDebug::LOG_TRACE_MEM);
 	if(not target.begin()) {
 		XPCC_LOG_ERROR << "failed to connect to target" << xpcc::endl;
 		XPCC_LOG_ERROR << "will just keep looping...." << xpcc::endl;
@@ -36,6 +36,16 @@ main()
 			;
 	}
 
+	// STM32F407 specific reads
+	XPCC_LOG_INFO << "Unique Device Id: 0x";
+	uint32_t data[3];
+	target.memLoad(0x1FFF7A10, data, 3);
+	XPCC_LOG_INFO << xpcc::hex << data[0] << data[1] << data[2] << xpcc::endl;
+
+	XPCC_LOG_INFO << "Flash Size: ";
+	uint32_t flash_size = 0;
+	target.memLoad(0x1FFF7A22, flash_size);
+	XPCC_LOG_INFO << (flash_size >> 16) << "kBytes" << xpcc::endl;
 	while (1)
 	{
 	}
